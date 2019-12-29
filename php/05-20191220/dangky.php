d22d8beec6c7aae2e2f7a3d0721996c2fa250cf1
= <=>
2019-12-29 20:11:37 +0700

    update

--- php/05-20191220/dangky.php
+++ php/05-20191220/dangky.php
@@ -1,31 +1,31 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password ="";
-// Kết nối database formData
-$connect = mysqli_connect($server, $username, $password);
-//Nếu kết nối bị lỗi thì xuất báo lỗi và thoát.
-if (!$connect) {
-    die("Không kết nối :" . mysqli_error($connect));
-    exit();
-}
-//Khai báo giá trị ban đầu, nếu không có thì khi chưa submit câu lệnh insert sẽ báo lỗi
-$email = "";
-$pass = "";
-$role_id = "";
-//Lấy giá trị POST từ form vừa submit
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(isset($_POST["email"])) { $email = $_POST['email']; }
-    if(isset($_POST["pass"])) { $pass = $_POST['pass']; }
-    if(isset($_POST["role_id"])) { $role_id = $_POST['role_id']; }
-}
-    $sql = "INSERT INTO 05_php22_JOIN.users (email, pass, role_id)
-    VALUES ('$email', '$pass', '$role_id')";
-        if (mysqli_query($connect, $sql)) {
-            echo "Them data thành công vào DATABASE";
-        } else {
-            echo "Them data thất bại: " . mysqli_error($connect);
-        };
+    $server = "localhost";
+    $username = "root";
+    $password ="";
+    // Kết nối database formData
+    $connect = mysqli_connect($server, $username, $password);
+    //Nếu kết nối bị lỗi thì xuất báo lỗi và thoát.
+    if (!$connect) {
+        die("Không kết nối :" . mysqli_error($connect));
+        exit();
+    }
+    //Khai báo giá trị ban đầu, nếu không có thì khi chưa submit câu lệnh insert sẽ báo lỗi
+    $email = "";
+    $pass = "";
+    $role_id = "";
+    //Lấy giá trị POST từ form vừa submit
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(isset($_POST["email"])) { $email = $_POST['email']; }
+        if(isset($_POST["pass"])) { $pass = $_POST['pass']; }
+        if(isset($_POST["role_id"])) { $role_id = $_POST['role_id']; }
+    }
+        $sql = "INSERT INTO 05_php22_JOIN.users (email, pass, role_id)
+        VALUES ('$email', '$pass', '$role_id')";
+            if (mysqli_query($connect, $sql)) {
+                echo "Them data thành công vào DATABASE";
+            } else {
+                echo "Them data thất bại: " . mysqli_error($connect);
+            };
 ?>
 <form action="./05-20191226/baitapvenha/baitap.php" method="POST">
 <input type="email" name="email" placeholder="Email...">
@@ -35,20 +35,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <br>Roles: <br>
 <select name="role_id">
 <?php
-$sql = "SELECT * FROM 05_php22_JOIN.roles";
-$result = mysqli_query($connect, $sql);
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-while ($row = mysqli_fetch_assoc($result)) {
+    $sql = "SELECT * FROM 05_php22_JOIN.roles";
+    $result = mysqli_query($connect, $sql);
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while ($row = mysqli_fetch_assoc($result)) {
 ?>
 
     <option value="<?php echo $row['id']; ?>">
         <?php echo $row['name']; ?>
     </option>
     <?php }
-    } else {
-    echo "0 results";
-    }
+        } else {
+        echo "0 results";
+        }
     ?>
 </select>
 <br>
@@ -56,6 +56,6 @@ while ($row = mysqli_fetch_assoc($result)) {
 <button type="submit">Đăng ký</button>
 </form>
 <?php
-//Đóng data base
-$connect->close();
+    //Đóng data base
+    $connect->close();
 ?>
--- php/05-20191220/list-user.php
+++ php/05-20191220/list-user.php
@@ -9,26 +9,26 @@
     <tbody>
     
 <?php
-$server = "localhost";
-$username = "root";
-$password ="";
-$dbname = "05_php22_JOIN";
-// Kết nối database 05_php21_JOIN
-$connect = mysqli_connect($server, $username, $password, $dbname);
-$sql = "select * from users join roles on users.role_id = roles.id";
-$result = mysqli_query($connect, $sql);
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" .$row['role_id'];
-    echo "<td>" .$row['email'];
-    echo "<td>" .$row['pass'];
-    echo "<td>" .$row['name'];
-}
-    } else {
-    echo "0 results";
-    };
+    $server = "localhost";
+    $username = "root";
+    $password ="";
+    $dbname = "05_php22_JOIN";
+    // Kết nối database 05_php21_JOIN
+    $connect = mysqli_connect($server, $username, $password, $dbname);
+    $sql = "select * from users join roles on users.role_id = roles.id";
+    $result = mysqli_query($connect, $sql);
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" .$row['role_id'];
+        echo "<td>" .$row['email'];
+        echo "<td>" .$row['pass'];
+        echo "<td>" .$row['name'];
+    }
+        } else {
+        echo "0 results";
+        };
 ?>
     </tbody>
 </table>
